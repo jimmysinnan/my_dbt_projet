1765dbf713d0aea97790172c26e501b0b85b79fd
jimmy-olivier SINNAN <oref-etude@agefma.fr>
2024-11-23 01:54:38 +0000

    convertir la colonne shipped_date en date

--- models/local_bike_models/staging/stg_local_bike_order.sql
+++ models/local_bike_models/staging/stg_local_bike_order.sql
@@ -2,13 +2,15 @@
     materialized = 'table'
 ) }}
 
+{{ config(materialized='table') }}
+
 SELECT
     order_id,
-    customer_id
+    customer_id,
     order_status,
     order_date,
     required_date,
-    cast(shipped_date as date) as shipped_date
+    shipped_date,
     store_id,
     staff_id
-from {{source("local_bike",'order')}}
+FROM {{ source("local_bike", "order") }}
