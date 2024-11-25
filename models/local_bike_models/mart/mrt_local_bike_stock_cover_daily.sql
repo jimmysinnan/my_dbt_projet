d33ef6da0d9198bd575b8ef25d3fb582017a3506
jimmy-olivier SINNAN <oref-etude@agefma.fr>
2024-11-25 16:51:23 +0000

    correction nom de table

--- models/local_bike_models/mart/mrt_local_bike_stock_cover_daily.sql
+++ models/local_bike_models/mart/mrt_local_bike_stock_cover_daily.sql
@@ -10,6 +10,6 @@ SELECT
     ROUND(coalesce(avg_daily_quantity_sold,0),2) as avg_daily_quantity_sold,
     ROUND(coalesce(stock_coverage_days,0),2) as stock_coverage_days
 FROM
-    stock_coverage
+    {{ref('int_local_bike_stock_cover_daily')}}
 ORDER BY
     stock_coverage_days ASC
